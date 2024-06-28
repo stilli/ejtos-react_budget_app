b23719bc4a4578a3338d2eaff5312bd267fdb92e
“stilli” <stilli@users.noreply.github.com>
2024-06-28 10:16:18 -0400

    Write budget change to redux storage

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,11 +2,15 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const { currency } = useContext(AppContext);
+    const { budget, currency, dispatch } = useContext(AppContext);
+    
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: event.target.value,
+        });
     }
     return (
 <div className='alert alert-secondary'>
