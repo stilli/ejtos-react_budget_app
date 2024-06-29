d27c44793a6696550b07b3b4249b1e37b8c7ff8d
stilli <stilli@users.noreply.github.com>
2024-06-29 08:38:46 +0200

    Make currency changeable in full app

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ import { FaPlusCircle, FaTimesCircle } from "react-icons/fa";
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+    const { dispatch,currency } = useContext(AppContext);
 
     const handleDeleteExpense = () => {
         dispatch({
@@ -28,7 +28,7 @@ const ExpenseItem = (props) => {
     return (
         <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
+        <td>{currency}{props.cost}</td>
         <td><FaPlusCircle size='1.5em' color='green' onClick={event=> increaseAllocation(props.name)}></FaPlusCircle></td>
         <td><FaTimesCircle size='1.5em' color='red' onClick={handleDeleteExpense}></FaTimesCircle></td>
         </tr>
